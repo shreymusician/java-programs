62435d4ffa5caf563ae5feaead83dcce5cb0bd30
Shreyas <shreyrangadhol@gmail.com>
2026-09-17 10:04:47 +0530

    4th program

---
+++ border_box_star.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter n : ";
+    cin >> n;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if(i==1 or i==n or j==1 or j==n)
+            {
+                cout << "* ";
+            }
+
+            else
+            {
+                cout << "  ";
+            }
+        }
+
+        cout << '\n';
+    }
+}
