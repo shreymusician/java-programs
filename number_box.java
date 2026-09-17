9e58c1947de953de94c160a2c3cfcac12649626b
Shreyas <shreyrangadhol@gmail.com>
2026-09-17 09:55:06 +0530

    Number box program

---
+++ number_box.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+    for(int r=1; r<=5; r++)
+    {
+        for(int i=1; i<=5; i++) {
+            System.out.print(r+" ");
+        }
+        System.out.print('\n');
+    }
+}
+
