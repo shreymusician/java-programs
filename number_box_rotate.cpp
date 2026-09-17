e119679f76242a023afca4bb2294a10b38559aca
Shreyas <shreyrangadhol@gmail.com>
2026-09-17 10:09:31 +0530

    6th program

---
+++ number_box_rotate.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int r=1; r<=5; r++)
+    {
+        for(int i=0; i<5; i++)
+        {
+            cout << r+i << " ";
+        }
+        cout << endl;
+    }
+}
