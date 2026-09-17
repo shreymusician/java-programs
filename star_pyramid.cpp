11475c3b6a876818c929e055b16d2242bb4396c6
Shreyas <shreyrangadhol@gmail.com>
2026-09-17 10:13:52 +0530

    9th program

---
+++ star_pyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int r=1; r<=5; r++)
+    {
+        for(int i=1; i<=r; i++)
+        {
+            cout << "* ";
+        }
+
+        for(int i=5-r; i>=0; i--)
+        {
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
