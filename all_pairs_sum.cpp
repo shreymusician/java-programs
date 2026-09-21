58ca307dbc278945c52bda9d02b7009986b84254
Shreyas <shreyrangadhol@gmail.com>
2026-09-21 14:47:47 +0530

    Pairs Sum Array Program

---
+++ all_pairs_sum.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, k;
+
+    cout << "Enter size of array : ";
+    cin >> n;
+
+    cout << "Enter k : ";
+    cin >> k;
+
+    int a[n];
+
+    cout << "Enter array elements : ";
+
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << "Pairs : \n";
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            if(a[i] + a[j] == k)
+                cout << "(" << a[i] << "," << a[j] << ")" << endl;
+        }
+    }
+
+   
+}
